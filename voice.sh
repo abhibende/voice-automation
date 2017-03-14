afeb207cbfc37b4e5e83855af36bdc88691fcba9
abhibende <abhinav.bende@gmail.com>
2017-03-14 10:36:54 +0530

    Added command to lock the screen

--- voice.sh
+++ voice.sh
@@ -8,5 +8,6 @@ case $cmd in
 *hostname*) echo "Your hostname is `hostname`" ;;
 *date*) echo "Today's date is `date +\"%A %d %B %Y\"`" ;;
 *time*) echo "It's `date +\"%H %M\"`";;
-*address|ip*) echo "Your ip address is `ip r l | grep -o "src.*" | awk '{print $2}'`"
+*address|ip*) echo "Your ip address is `ip r l | grep -o "src.*" | awk '{print $2}'`";;
+*lock*)gnome-screensaver-command -l ;;
 esac
